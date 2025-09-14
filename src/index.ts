0e131f6d4df9781f589b5ff11c556aef91f0c2bd
Ahmad Amr <gamerteacher12@gmail.com>
2025-09-14 15:29:10 +0300

    test : serverless done

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import express  from "express";
+import serverless from "serverless-http";
 import {config} from "dotenv";
 import { controllerHandler } from "./Utiles";
 import { databaseConnection } from "./DB/connection";
 config();
 
+let dbPromise: Promise<void> | null = null;
+function connectOnce() {
+    if (!dbPromise) dbPromise = databaseConnection();
+    return dbPromise;
+  }
 
 const app:express.Application = express();
-const port:number | string = process.env.PORT || 3000;
-databaseConnection();
+// const port:number | string = process.env.PORT || 3000;
+// databaseConnection();
 controllerHandler(app);
 
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-})
+// app.listen(port,()=>{
+//     console.log(`Server running on port ${port}`);
+// })
+
+const handler = serverless(app);
+export default async (req: any, res: any) => {
+    await connectOnce();      // ensure DB is ready
+    return handler(req, res); // delegate to Express-as-a-function
+  };
